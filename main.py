27002013b51bb10e5fa94ced927799dcdc78f7a3
Hao Hoang <hajuha168@gmail.com>
2021-12-28 12:46:41 +0700

    log

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ def dga_daily():
 schedule.every().hour.do(dga_claim)
 schedule.every().day.at("21:30").do(dga_daily)
 discord_automessage.sendMessage(888653476169666571,"This is a test message for @Yuhakez#0641 ")
+discord_automessage.sendMessage(888653476169666571,"Auto claim start in")
+time.sleep(1)
+discord_automessage.sendMessage(888653476169666571,"3")
+time.sleep(1)
+discord_automessage.sendMessage(888653476169666571,"2")
+time.sleep(1)
+discord_automessage.sendMessage(888653476169666571,"1")
 while 1:    
     schedule.run_pending()
     time.sleep(1)
